c16586ef0ef59602f3990f2b8e1c31889d56ba85
damianhol <holub.damian1@gmail.com>
2022-09-28 13:55:18 +0200

    update

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'MSI Laptop',
+        productNumber: '42131',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Microphone',
+        productNumber: '88231',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Headphones',
+        productNumber: '73531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mobile Phone',
+        productNumber: '87131',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Prone Drone',
+        productNumber: '92431',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'PC OMG',
+        productNumber: '72131',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+];
